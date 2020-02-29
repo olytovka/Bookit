5e43f32475f1720749115d1f50b272a4bbcb23d3
Olha <58573698+olytovka@users.noreply.github.com>
2020-02-29 15:51:09 -0500

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -3,5 +3,7 @@ package test;
 public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Excuse me! Pay attention please!");
+        
+        System.out.println("Goodbye git"); // this is from github 
     }
 }
