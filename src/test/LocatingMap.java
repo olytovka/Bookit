49d2ae9829154884e2d0e7e2b9087271f9caa05d
Olha Lytovka <olha@Olhas-MacBook-Air.local>
2020-02-29 15:57:35 -0500

    LocatingMap.java

--- src/test/LocatingMap.java
+++ src/test/LocatingMap.java
@@ -1,4 +1,9 @@
 package test;
 
 public class LocatingMap {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello Amirjon from line 7");
+    }
 }
--- src/test/Login.java
+++ src/test/Login.java
@@ -2,7 +2,7 @@ package test;
 
 public class Login {
     public static void main(String[] args) {
-        System.out.println("user logged in");
+        System.out.println("user logged ini");
         
         //adding info at GitHub
     }
